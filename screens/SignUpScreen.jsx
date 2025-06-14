50d2d39f10c6e8feb83f821d7cae62cc652869eb
Ayush750502 <ayush750502@gmail.com>
2025-06-14 09:45:03 +0530

    adding name and confirm password fields and adding code accordingly

--- screens/SignUpScreen.jsx
+++ screens/SignUpScreen.jsx
@@ -10,10 +10,13 @@ import styles from "../style";
 import messages from "../messages";
 
 const SignUpSchema = Yup.object().shape({
+  name: Yup.string()
+    .max(30, messages.Validate.SignUp.name.length)
+    .required(messages.Validate.SignUp.name.required),
   email: Yup.string()
     .email(messages.Validate.SignUp.email.email)
     .max(50, messages.Validate.SignUp.email.length)
-    .required("Required"),
+    .required(messages.Validate.SignUp.email.required),
   password: Yup.string()
     .min(4, messages.Validate.SignUp.password.minLength)
     .max(20, messages.Validate.SignUp.password.maxLength)
@@ -21,7 +24,10 @@ const SignUpSchema = Yup.object().shape({
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d])/,
         messages.Validate.SignUp.password.charaterSpecification
       )
-    .required("Required"),
+    .required(messages.Validate.SignUp.password.required),
+    confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], messages.Validate.SignUp.confirmPassword.match)
+    .required(messages.Validate.SignUp.confirmPassword.required),
 });
 
 export default function SignUpScreen({ navigation }) {
@@ -32,18 +38,20 @@ export default function SignUpScreen({ navigation }) {
       const storedUsers = await AsyncStorage.getItem("users");
       const users = storedUsers ? JSON.parse(storedUsers) : [];
 
-      // Check if user already exists
-      const userExists = users.some((u) => u.email === values.email);
+      const userExists = users.some((u) => u.name === values.name);
       if (userExists) {
-        Alert.alert(messages.Alerts.SignUp.UserFound.Title,messages.Alerts.SignUp.UserFound.message);
+        Alert.alert(messages.Alerts.SignUp.UserFound.Title, messages.Alerts.SignUp.UserFound.message);
         return;
       }
 
-      // Add new user
-      users.push({ email: values.email, password: values.password });
+      users.push({
+        name: values.name,
+        email: values.email,
+        password: values.password,
+      });
+
       await AsyncStorage.setItem("users", JSON.stringify(users));
-      dispatch(login(values.email)); 
-      Alert.alert(messages.Alerts.SignUp.Success.Title, messages.Alerts.SignUp.Success.message);
+      dispatch(login(values.name));
       navigation.replace("Login");
     } catch (error) {
       console.error(error);
@@ -59,12 +67,22 @@ export default function SignUpScreen({ navigation }) {
 >
       <Text style={styles.signUpTitle}>Sign Up</Text>
       <Formik
-        initialValues={{ email: "", password: "" }}
+        initialValues={{ name: "", email: "", password: "", confirmPassword: "" }}
         validationSchema={SignUpSchema}
         onSubmit={handleSignUp}
       >
         {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
           <>
+            <TextInput
+                style={styles.signUpInput}
+                placeholder="Name"
+                onChangeText={handleChange("name")}
+                onBlur={handleBlur("name")}
+                value={values.name}
+              />
+              {errors.name && touched.name && (
+                <Text style={styles.signUpError}>{errors.name}</Text>
+              )}
             <TextInput
               style={styles.signUpInput}
               placeholder="Email"
@@ -88,6 +106,17 @@ export default function SignUpScreen({ navigation }) {
             {errors.password && touched.password && (
               <Text style={styles.signUpError}>{errors.password}</Text>
             )}
+            <TextInput
+                style={styles.signUpInput}
+                placeholder="Confirm Password"
+                onChangeText={handleChange("confirmPassword")}
+                onBlur={handleBlur("confirmPassword")}
+                value={values.confirmPassword}
+                secureTextEntry
+              />
+              {errors.confirmPassword && touched.confirmPassword && (
+                <Text style={styles.signUpError}>{errors.confirmPassword}</Text>
+              )}
             <View style={styles.signUpButtonContainer} >
             <TouchableOpacity
               style={[styles.loginScreenButtons, {backgroundColor: 'blue'}]}
